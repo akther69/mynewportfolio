9f56da38157fcf7179bcccdbd3a43d96430a6649
suhaib akther s <ssuhaibakther12@gmail.com>
2025-02-23 21:52:16 +0530

    portfolio

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -26,11 +26,6 @@ function Index() {
       <Nav.Item>
         <Nav.Link eventKey="link-2">Link</Nav.Link>
       </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
     </Nav>
 
 
@@ -50,31 +45,31 @@ function Index() {
       <div className="container">
         <div className="row">
           <h1 className='text-center mt-5 text-light' style={{ letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>About Me</h1>
-          <h5 className='text-center text-light my-5'>Discover more about who I am, what I do, and the programming and technological skills I bring to the table.</h5>
+          <h5 className='text-center text-light my-5' style={{fontFamily: "'Poppins', sans-serif"}}>Discover more about who I am, what I do, and the programming and technological skills I bring to the table.</h5>
         </div>
         <div className="container p-5">
           <div className="row">
           <div className="col-6">
-            <h1 className='text-light'>Know more about me</h1>
-            <p className='text-light ' style={{ fontFamily: "'Space Grotesk', sans-serif" }}>I’m a <b>Python-Django full-stack developer from Nilgiris, Tamil Nadu</b>, with a BE in Computer Science and experience building dynamic, user-friendly web applications. I focus on creating scalable back-end systems using Django and Django REST Framework and designing responsive front-end interfaces with HTML, CSS, and Tailwind CSS. I’ve worked on projects like e-commerce platforms and task management tools, which have helped me strengthen my problem-solving skills and learn about building real-world applications.
-               Right now, I’m working as an RPA Developer at IntelliJohn Labs in Kochi. I use UiPath to automate repetitive processes, making workflows faster and more efficient. This role has been exciting because I get to combine my love for technology with practical automation solutions.
-               I’m always looking to learn new things and take on projects that challenge me to grow while making an impact.</p>
+            <h1 className='text-light' style={{fontFamily: "'Poppins', sans-serif"}}>Know more about me</h1>
+            <p className='text-light ' style={{ fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>I'm a Python-Django full-stack developer from Nilgiris, Tamil Nadu, with a BE in Computer Science. I love building dynamic web applications, focusing on scalable back-end systems with Django and responsive front-end interfaces using Tailwind CSS. I've worked on e-commerce platforms and task management tools, which have sharpened my problem-solving skills.Right now, I'm an RPA Developer at IntelliJohn Labs in Kochi, using UiPath to automate workflows and make processes more efficient. I'm always eager to learn new things and take on projects that push me to grow and make a real impact.</p>
                <div>
                 <button className='btn btn-light'>Contact</button>
                </div>
           </div>
           <div className="col-1"></div>
           <div className="col-5">
-            <h2 className='text-light'>Skills</h2>
+            <h2 className='text-light' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Skills</h2>
             <button className='btn btn-light me-2 my-2'>Python</button>
             <button className='btn btn-light me-2 my-2'>Django</button>
             <button className='btn btn-light me-2 my-2'>Django rest FrameWork</button>
             <button className='btn btn-light me-2 my-2'>Html</button>
-            <button className='btn btn-light me-2 my-2'>Css</button>
+            <button className='btn btn-light me-2 my-2'>CSS</button>
             <button className='btn btn-light me-2 my-2'>MySql</button>
             <button className='btn btn-light me-2 my-2'>Bootstrap</button>
             <button className='btn btn-light me-2 my-2'>React</button>
             <button className='btn btn-light me-2 my-2'>JavaScript</button>
+            <button className="btn btn-light me-2 my-2" style={{ transition: "transform 0.3s ease-in-out" }}onMouseEnter={(e) => (e.target.style.transform = "scale(1.5)")}onMouseLeave={(e) => (e.target.style.transform = "scale(1)")}>Python</button>
+
           </div>
           </div>
         </div>
@@ -83,9 +78,9 @@ function Index() {
 
     {/* timeline */}
 
-    <section style={{ height: "140vh", backgroundColor: "#9db2bf"}} className='p-5'>
+    <section style={{ height: "150vh", backgroundColor: "#9db2bf"}} className='p-4'>
     <div>
-      <h1 className='text-center p-3' style={{color: "#2a3335", letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>Time Line</h1>
+      <h1 className='text-center p-4' style={{color: "#2a3335", letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>Time Line</h1>
       <VerticalTimeline>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -93,8 +88,8 @@ function Index() {
               iconStyle={{ background: "#2a3335", color: "#2a3335" }}
               contentStyle={{ color: "#2a3335"}}
             >
-              <h3 className="vertical-timeline-element-title">Graduated; B.E in Computer Science And Engineering</h3>
-              <p>Hindusthan College of Engineering And Technology Coimbatore</p>
+              <h3 className="vertical-timeline-element-title" style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Graduated; B.E in Computer Science And Engineering</h3>
+              <p style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Hindusthan College of Engineering And Technology Coimbatore</p>
             </VerticalTimelineElement>
       
             <VerticalTimelineElement
@@ -104,8 +99,8 @@ function Index() {
               contentStyle={{ color: "#2a3335"}}
 
             >
-              <h3 className="vertical-timeline-element-title">Started Internship as Python-web Development Expert</h3>
-              <p>Joined Luminar Technolab</p>
+              <h3 className="vertical-timeline-element-title" style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Started Internship as Python-web Development Expert</h3>
+              <p style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Joined Luminar Technolab</p>
             </VerticalTimelineElement>
       
             <VerticalTimelineElement
@@ -113,8 +108,8 @@ function Index() {
               date ="2024-11-18"
               iconStyle={{ background: "#2a3335", color: "#fff" }}
             >
-              <h3 className="vertical-timeline-element-title">Completed Internship</h3>
-              <p>Luminar Technolab</p>
+              <h3 className="vertical-timeline-element-title" style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Completed Internship</h3>
+              <p style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Luminar Technolab</p>
             </VerticalTimelineElement>
 
             
@@ -127,8 +122,8 @@ function Index() {
               contentStyle={{ color: "#2a3335"}}
 
             >
-              <h3 className="vertical-timeline-element-title" style={{color:"#2a3335"}}>Joined as RPA Developer</h3>
-              <p>Intellijohn Labs</p>
+              <h3 className="vertical-timeline-element-title" style={{color:"#2a3335",fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Joined as RPA Developer</h3>
+              <p style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Intellijohn Labs</p>
             </VerticalTimelineElement>
 
             
@@ -176,27 +171,27 @@ function Index() {
 
           </div>
           <div className="col-6">
-            <div   style={{ marginTop: '60px' }}>
+            <div   style={{ marginTop: '60px' ,fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Pix & Words</h1>
               <p>Pix & Words is a social media platform where users can share images, videos, and text posts. It features likes, comments, real-time updates, and customizable profiles for a personalized and engaging experience.</p>
               <button className='btn btn-danger'>View on GitHub</button>
 
             </div >
-            <div   style={{ marginTop: '230px' }}><h1>Venus Fancy</h1>
+            <div   style={{ marginTop: '190px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}><h1>Venus Fancy</h1>
             <p>The web application is an e-commerce platform for purchasing accessories like necklaces, bangles, and rings. Built with Django, Tailwind CSS, and DevUI, it offers a smooth, user-friendly shopping experience with essential e-commerce features.</p>
             <button className='btn btn-danger'>View on GitHub</button>
             </div>
-            <div   style={{ marginTop: '230px' }}>
+            <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>To-Do Web Application</h1>
               <p>A task management app with login functionality, enabling users to create, update, and delete tasks. Developed using Django, HTML, CSS, and SQLite for efficient task tracking.</p>
               <button className='btn btn-danger'>View on GitHub</button>
             </div>
-            <div   style={{ marginTop: '230px' }}>
+            <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Expense Manager</h1>
               <p>An expense tracking app that visualizes monthly spending with graphs by category and payment method. It includes a REST API and email/SMS notifications to help users stay on top of their finances.</p>
               <button className='btn btn-danger'>View on GitHub</button>
             </div>
-            <div   style={{ marginTop: '220px' }}>
+            <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Netflix Clone (Front-End)</h1>
               <p>A front-end Netflix clone built to showcase streaming UI, featuring an attractive layout with browsing and preview options. Designed to replicate the user experience of a streaming platform.</p>
               <button className='btn btn-danger'>View on GitHub</button>
