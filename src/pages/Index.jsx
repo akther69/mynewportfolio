4f70ad4d09ff78d0dbe1f40fde1ec2711e8940a9
suhaib akther s <ssuhaibakther12@gmail.com>
2025-02-24 13:14:16 +0530

    portfolio

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,74 +1,97 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav';
 import "react-vertical-timeline-component/style.min.css";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub, faWhatsapp } from "@fortawesome/free-brands-svg-icons"; 
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { color } from 'framer-motion';
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { faArrowUp,faArrowDown } from "@fortawesome/free-solid-svg-icons"; 
 
 
 
 
 function Index() {
+  function scrollToTop() {
+    document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
+  }
+  function scrollToContact() {
+    document.getElementById("contactdetails").scrollIntoView({ behavior: "smooth" });
+  }
+  
   return (
     <div>
 
       {/* navbar */}
-        <Nav fill variant="tabs" defaultActiveKey="/home">
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-    </Nav>
+      <Navbar id='navbar'  bg="light" data-bs-theme="light">
+        <Container>
+          <Nav>
+          <Nav.Link href="#profile">
+            <img src="./src/images/myphoto.jpeg (1).jpg" alt="Profile" className="rounded-circle" style={{ width: "40px", height: "40px", objectFit: "cover" }}/>
+            <label className='mx-3' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>SUHAIB AKTHER S</label>
+          </Nav.Link>
+        </Nav>
+          <Nav className="">
+          <Nav.Link href="#home" className='me-5 mx-2 text-dark' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>HOME</Nav.Link>
+            <Nav.Link href="#aboutme" className='me-5 mx-2 text-dark' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>ABOUT ME</Nav.Link>
+            <Nav.Link href="#mytimeline" className='me-5 mx-2 text-dark' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>MY TIME LINE</Nav.Link>
+            <Nav.Link href="#projects" className='me-5 mx-2 text-dark' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>PROJECTS</Nav.Link>
+            <Nav.Link href="#contactdetails" className='me-5 mx-2 text-dark' style={{fontFamily: "'Poppins', sans-serif",fontWeight: "bold"}}>CONTACT DETAILS</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
 
     {/* into  */}
-    <section style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#27374d"}}>
+    <section id='home' style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#27374d"}}>
         <div className="container">
             <div className="row">
-                <h3 className='text-center text-light' style={{ letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>👋,I'm SUHAIB AKTHER S</h3>
-                <h3 className="text-center mt-5 text-light">PYTHON FULL STACK DEVELOPER</h3>
+                <h3 className='text-center text-light' style={{ letterSpacing: "0.1em", lineHeight: "5rem",fontSize: "4.5rem",fontWeight: "bold"}}>👋,I'm SUHAIB AKTHER S</h3>
+                <h6 className="text-center mt-5 text-light mb-3" style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>I’m a Python-Django full-stack developer, building web apps with clean and efficient code.</h6>
+                <div className='text-center mt-3'>
+                <button className='btn btn-light rounded me-2'>
+                <a href="https://www.linkedin.com/in/suhaib-akther-s-baa753233?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BKTmeq42WTQ%2BnuY7ythGSuA%3D%3D" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="1x" className="mx-2 text-dark hover:text-blue-800" /></a>LinkedIn
+                </button>
+                <button className='btn btn-light me-2'><a href="https://github.com/akther69" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="1x" className="mx-2 text-dark hover:text-gray-700" /></a>GitHub</button>
+                <button className='btn btn-light'><a href="mailto:ssuhaibakther12@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} size="1x" className="mx-2 text-dark hover:text-red-800" /></a>Mail</button>
+                </div>
             </div>
+            <button onClick={scrollToTop} className="btn btn-dark up-button"><FontAwesomeIcon icon={faArrowUp} /></button>
+            <button onClick={scrollToContact} className="btn btn-dark down-button"><FontAwesomeIcon icon={faArrowDown} /></button>
         </div>
     </section>
 
 
     {/* aboutme */}
-    <section style={{ height: "100vh",backgroundColor:"#526d82"}}>
+    <section id='aboutme' style={{ height: "100vh",backgroundColor:"#526d82"}}>
       <div className="container">
         <div className="row">
           <h1 className='text-center mt-5 text-light' style={{ letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>About Me</h1>
-          <h5 className='text-center text-light my-5' style={{fontFamily: "'Poppins', sans-serif"}}>Discover more about who I am, what I do, and the programming and technological skills I bring to the table.</h5>
+          <h5 className='text-center text-light mt-4 mb-3' style={{fontFamily: "'Poppins', sans-serif"}}>Discover more about who I am, what I do, and the programming and technological skills I bring to the table.</h5>
         </div>
         <div className="container p-5">
           <div className="row">
           <div className="col-6">
-            <h1 className='text-light' style={{fontFamily: "'Poppins', sans-serif"}}>Know more about me</h1>
-            <p className='text-light ' style={{ fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>I'm a Python-Django full-stack developer from Nilgiris, Tamil Nadu, with a BE in Computer Science. I love building dynamic web applications, focusing on scalable back-end systems with Django and responsive front-end interfaces using Tailwind CSS. I've worked on e-commerce platforms and task management tools, which have sharpened my problem-solving skills.Right now, I'm an RPA Developer at IntelliJohn Labs in Kochi, using UiPath to automate workflows and make processes more efficient. I'm always eager to learn new things and take on projects that push me to grow and make a real impact.</p>
+            <h1 className='text-light' style={{fontFamily: "'Poppins', sans-serif"}}><b>Know more about me</b></h1>
+            <p className='text-light ' style={{ fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em",lineHeight:"2"}}>I'm a Python-Django Full Stack Developer from Nilgiris, Tamil Nadu, with a BE in Computer Science. I specialize in building dynamic web applications, focusing on scalable back-end systems with Django and responsive front-end interfaces. My experience includes working on various projects that have strengthened my problem-solving skills. Currently, I work as an RPA Developer at IntelliJohn Labs in Kochi, leveraging UiPath to automate workflows and optimize processes. Passionate about continuous learning, I thrive on tackling challenging projects that drive growth and create meaningful impact.</p>
                <div>
-                <button className='btn btn-light'>Contact</button>
+                <a href="#contactdetails"><button className='btn btn-light'>Contact</button></a>
+                
                </div>
           </div>
           <div className="col-1"></div>
           <div className="col-5">
-            <h2 className='text-light' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Skills</h2>
-            <button className='btn btn-light me-2 my-2'>Python</button>
-            <button className='btn btn-light me-2 my-2'>Django</button>
-            <button className='btn btn-light me-2 my-2'>Django rest FrameWork</button>
-            <button className='btn btn-light me-2 my-2'>Html</button>
-            <button className='btn btn-light me-2 my-2'>CSS</button>
-            <button className='btn btn-light me-2 my-2'>MySql</button>
-            <button className='btn btn-light me-2 my-2'>Bootstrap</button>
-            <button className='btn btn-light me-2 my-2'>React</button>
-            <button className='btn btn-light me-2 my-2'>JavaScript</button>
-            <button className="btn btn-light me-2 my-2" style={{ transition: "transform 0.3s ease-in-out" }}onMouseEnter={(e) => (e.target.style.transform = "scale(1.5)")}onMouseLeave={(e) => (e.target.style.transform = "scale(1)")}>Python</button>
+            <h2 className='text-light' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}><b>Skills</b></h2>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Python</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Django</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Html</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Django rest FrameWork</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>CSS</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>MySql</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Bootstrap</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>React</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>JavaScript</button>
+            <button className='btn btn-light me-2 my-2 p-3 px-3' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>UiPath</button>
+
 
           </div>
           </div>
@@ -78,9 +101,9 @@ function Index() {
 
     {/* timeline */}
 
-    <section style={{ height: "150vh", backgroundColor: "#9db2bf"}} className='p-4'>
+    <section id='mytimeline' style={{ height: "150vh", backgroundColor: "#9db2bf"}} className='p-4'>
     <div>
-      <h1 className='text-center p-4' style={{color: "#2a3335", letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>Time Line</h1>
+      <h1 className='text-center p-4' style={{color: "#2a3335", letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>My Time Line</h1>
       <VerticalTimeline>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -134,7 +157,7 @@ function Index() {
 
 
     {/* project */}
-    <section style={{ backgroundColor: "#dde6ed"}}>
+    <section id='projects' style={{ backgroundColor: "#dde6ed"}}>
       <div className="container">
         <div className="row">
           <h1 className='text-center my-5' style={{ letterSpacing: "0.1em", lineHeight: "4rem",fontSize: "4.5rem",fontWeight: "bold"}}>Projects</h1>
@@ -174,38 +197,59 @@ function Index() {
             <div   style={{ marginTop: '60px' ,fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Pix & Words</h1>
               <p>Pix & Words is a social media platform where users can share images, videos, and text posts. It features likes, comments, real-time updates, and customizable profiles for a personalized and engaging experience.</p>
+              <a href="https://github.com/akther69/pix-words.git" target="_blank" rel="noopener noreferrer">
               <button className='btn btn-danger'>View on GitHub</button>
+              </a>
 
             </div >
             <div   style={{ marginTop: '190px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}><h1>Venus Fancy</h1>
             <p>The web application is an e-commerce platform for purchasing accessories like necklaces, bangles, and rings. Built with Django, Tailwind CSS, and DevUI, it offers a smooth, user-friendly shopping experience with essential e-commerce features.</p>
-            <button className='btn btn-danger'>View on GitHub</button>
-            </div>
+            <a href="https://github.com/akther69/venus-fancy.git" target="_blank" rel="noopener noreferrer">
+              <button className='btn btn-danger'>View on GitHub</button>
+              </a>            </div>
             <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>To-Do Web Application</h1>
               <p>A task management app with login functionality, enabling users to create, update, and delete tasks. Developed using Django, HTML, CSS, and SQLite for efficient task tracking.</p>
+              <a href="https://github.com/akther69/todo.git" target="_blank" rel="noopener noreferrer">
               <button className='btn btn-danger'>View on GitHub</button>
+              </a>
+             
             </div>
             <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Expense Manager</h1>
               <p>An expense tracking app that visualizes monthly spending with graphs by category and payment method. It includes a REST API and email/SMS notifications to help users stay on top of their finances.</p>
+              <a href="https://github.com/akther69/expense-manager.git" target="_blank" rel="noopener noreferrer">
               <button className='btn btn-danger'>View on GitHub</button>
-            </div>
+              </a>            </div>
             <div   style={{ marginTop: '200px',fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>
               <h1>Netflix Clone (Front-End)</h1>
               <p>A front-end Netflix clone built to showcase streaming UI, featuring an attractive layout with browsing and preview options. Designed to replicate the user experience of a streaming platform.</p>
+              <a href="https://github.com/akther69/netflix.git" target="_blank" rel="noopener noreferrer">
               <button className='btn btn-danger'>View on GitHub</button>
-            </div>
+              </a>            </div>
           </div>
         </div>
-        <hr />
+      </div>
+
+      {/* resume download */}
+
+    <section>
+      <div className='text-center'>
+      <h3 className='text-center p-3' style={{fontFamily: "'Poppins', sans-serif"}}>Thanks for scrolling !  🙏✨</h3>
+      <a href="./src/pdf/suhaib_akther_s_resume.pdf"><button className='btn btn-outline-primary mb-5' style={{fontFamily: "'Poppins', sans-serif", letterSpacing: "0.1em"}}>Download Resume</button>
+      </a>
       </div>
     </section>
+    </section>
+
+
+
+
 
     {/* contact info */}
 
 
-    <section>
+    <section id='contactdetails'>
       <div className="container my-5">
         <div className="row">
           <div className="col-4"></div>
@@ -240,6 +284,9 @@ function Index() {
       </div>
 
     </section>
+
+
+    
 
 
     </div>
