1071fcce7bb36212e15f544a0794fe13d6e45bd9
suhaib akther s <ssuhaibakther12@gmail.com>
2025-02-24 20:37:10 +0530

    Fix error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mynewportfolio/",
-  build: {
-    outDir: "dist",
-    assetsInclude: ['**/*.mp4', '**/*.pdf'] // Explicitly include video/pdf assets
-  }
+  base: "/mynewportfolio/"
 });
